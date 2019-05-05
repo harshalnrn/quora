48af2fc070840c2c5e032a1784bafd9b2d63be3b
harshalnrn <harshalnrn@gmail.com>
2019-05-04 23:12:11 -0700

    correct merge issues in these 2 files

--- quora-api/src/main/java/com/upgrad/quora/api/restHandler/RestExceptionHandler.java
+++ quora-api/src/main/java/com/upgrad/quora/api/restHandler/RestExceptionHandler.java
@@ -1,7 +1,8 @@
-package com.upgrad.quora.api.restHandler;
+package com.upgrad.quora.api.exception;
 
 
 import com.upgrad.quora.api.model.ErrorResponse;
+import com.upgrad.quora.service.exception.*;
 import com.upgrad.quora.service.exception.AuthenticationFailedException;
 import com.upgrad.quora.service.exception.AuthorizationFailedException;
 import com.upgrad.quora.service.exception.SignOutRestrictedException;
@@ -15,28 +16,44 @@ import org.springframework.web.context.request.WebRequest;
 @ControllerAdvice
 public class RestExceptionHandler {
 
+  @ExceptionHandler(SignOutRestrictedException.class)
+  public ResponseEntity<ErrorResponse> resourceNotFoundException(
+      SignOutRestrictedException exe, WebRequest request) {
+    return new ResponseEntity<ErrorResponse>(
+        new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()),
+        HttpStatus.NOT_FOUND);
+  }
 
-    @ExceptionHandler(SignOutRestrictedException.class)
-    public ResponseEntity<ErrorResponse> resourceNotFoundException(SignOutRestrictedException exe, WebRequest request) {
-        return new ResponseEntity<ErrorResponse>(
-                new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()), HttpStatus.NOT_FOUND);
-    }
+  @ExceptionHandler(SignUpRestrictedException.class)
+  public ResponseEntity<ErrorResponse> resourceAlreadyExistsException(
+      SignUpRestrictedException exc, WebRequest webRequest) {
+    return new ResponseEntity<ErrorResponse>(
+        new ErrorResponse().code(exc.getCode()).message(exc.getErrorMessage()),
+        HttpStatus.CONFLICT);
+  }
 
+  @ExceptionHandler(AuthenticationFailedException.class)
+  public ResponseEntity<ErrorResponse> resourceNotFoundException(
+      AuthenticationFailedException exc, WebRequest webRequest) {
+    return new ResponseEntity<ErrorResponse>(
+        new ErrorResponse().code(exc.getCode()).message(exc.getErrorMessage()),
+        HttpStatus.UNAUTHORIZED);
+  }
 
+  @ExceptionHandler(AuthorizationFailedException.class)
+  public ResponseEntity<ErrorResponse> resourceUnauthorizedException(
+      AuthorizationFailedException exe, WebRequest webRequest) {
+    return new ResponseEntity<ErrorResponse>(
+        new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()),
+        HttpStatus.UNAUTHORIZED);
+  }
 
-    @ExceptionHandler(SignUpRestrictedException.class)
-    public ResponseEntity<ErrorResponse> resourceAlreadyExistsException(SignUpRestrictedException exc , WebRequest webRequest){
-        return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(exc.getCode()).message(exc.getErrorMessage()) , HttpStatus.CONFLICT);
-    }
-
-    @ExceptionHandler(AuthenticationFailedException.class)
-    public ResponseEntity<ErrorResponse> resourceNotFoundException(AuthenticationFailedException exc , WebRequest webRequest){
-        return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(exc.getCode()).message(exc.getErrorMessage()) , HttpStatus.UNAUTHORIZED);
-    }
-
-    @ExceptionHandler(AuthorizationFailedException.class)
-    public ResponseEntity<ErrorResponse> resourceNotFoundException(AuthorizationFailedException e,WebRequest webRequest){
-        return new ResponseEntity<ErrorResponse>(new ErrorResponse().code(e.getCode()).message(e.getErrorMessage()),HttpStatus.UNAUTHORIZED);
-    }
+  @ExceptionHandler(UserNotFoundException.class)
+  public ResponseEntity<ErrorResponse> resourceNotFoundException(
+      UserNotFoundException exe, WebRequest webRequest) {
+    return new ResponseEntity<ErrorResponse>(
+        new ErrorResponse().code(exe.getCode()).message(exe.getErrorMessage()),
+        HttpStatus.NOT_FOUND);
+  }
 }
 
