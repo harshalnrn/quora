e4cccfe8620e76e70f8e824be64dcd47ee05db76
Rajat0023 <rajatsingh2237@gmail.com>
2019-05-11 23:01:40 +0530

    Added GenericException Enum, made relevant changes to UserBusinessService,Formatting done

--- quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
+++ quora-service/src/main/java/com/upgrad/quora/service/business/UserBusinessService.java
@@ -4,6 +4,7 @@ import com.upgrad.quora.service.dao.UserDao;
 import com.upgrad.quora.service.entity.UserAuthTokenEntity;
 import com.upgrad.quora.service.entity.UserEntity;
 import com.upgrad.quora.service.exception.*;
+import com.upgrad.quora.service.common.GenericExceptionCode;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Propagation;
@@ -24,16 +25,18 @@ public class UserBusinessService {
 
   public UserEntity signUp(UserEntity userEntity) throws SignUpRestrictedException {
 
-    //Check if the user exists for given username
+    // Check if the user exists for given username
     UserEntity existingUserEntity = userDao.findUserByUserName(userEntity.getUsername());
-    if(existingUserEntity != null){
-      throw new SignUpRestrictedException("SGR-001" , "Try any other Username, this Username has already been taken");
+    if (existingUserEntity != null) {
+      throw new SignUpRestrictedException(
+          GenericExceptionCode.SGR_001.getCode(), GenericExceptionCode.SGR_001.getDescription());
     }
 
-    //Check if the user exists for given email
+    // Check if the user exists for given email
     existingUserEntity = userDao.findUserByEmail(userEntity.getEmail());
-    if(existingUserEntity != null){
-      throw new SignUpRestrictedException("SGR-002","This user has already been registered, try with any other emailId");
+    if (existingUserEntity != null) {
+      throw new SignUpRestrictedException(
+          GenericExceptionCode.SGR_002.getCode(), GenericExceptionCode.SGR_002.getDescription());
     }
 
     String[] encryptedText = passwordCryptographyProvider.encrypt(userEntity.getPassword());
@@ -43,11 +46,12 @@ public class UserBusinessService {
     return userDao.createUser(userEntity);
   }
 
-  public UserAuthTokenEntity userLogin(String username, String password) throws AuthenticationFailedException {
+  public UserAuthTokenEntity userLogin(String username, String password)
+      throws AuthenticationFailedException {
 
-        // validate username
-        UserEntity userEntity = userDao.findUserByUserName(username);
-    // no null check here . vimp to do null check on objects befor eusing them
+    // validate username
+    UserEntity userEntity = userDao.findUserByUserName(username);
+    // no null check here . vimp to do null check on objects before using them
     // encryptand validate password
     if (userEntity != null) {
       String encryptedPassword =
@@ -64,81 +68,110 @@ public class UserBusinessService {
         userAuthTokenEntity.setAccess_token(accessToken);
         userAuthTokenEntity.setUsers(userEntity);
         userAuthTokenEntity.setLoginAt(issuedTime);
-        //userAuthTokenEntity.setLogoutAt(expiryTime); shall be set after sign out
+        // userAuthTokenEntity.setLogoutAt(expiryTime); shall be set after sign out
         userAuthTokenEntity.setExpiresAt(expiryTime); // why 2 columns
         userAuthTokenEntity.setUuid(UUID.randomUUID().toString());
-        userDao.createAuthToken(userAuthTokenEntity);   // UserAuthtoken should be persisted in the DB for future reference
+        userDao.createAuthToken(
+            userAuthTokenEntity); // UserAuthtoken should be persisted in the DB for future
+        // reference
         return userAuthTokenEntity;
       } else {
-          throw new AuthenticationFailedException("ATH-002", "Password failed");
+        throw new AuthenticationFailedException(
+            GenericExceptionCode.ATH_002.getCode(), GenericExceptionCode.ATH_002.getDescription());
       }
-      }
-      else{
-        throw new AuthenticationFailedException("ATH-001", "This username does not exist");
-        }
+    } else {
+      throw new AuthenticationFailedException(
+          GenericExceptionCode.ATH_001.getCode(), GenericExceptionCode.ATH_001.getDescription());
     }
+  }
 
-
- //login-logout auditing is done in user-auth-token-entity
-    public UserAuthTokenEntity signOut(String access_token) throws SignOutRestrictedException {
-      UserAuthTokenEntity userAuthToken = userDao.getAuthToken(access_token); //Fetching authtoken entity
-      // Checking if the Access token entered matches the Access token in the DataBase and null check
-        if (userAuthToken!=null && access_token.equals(userAuthToken.getAccess_token()))   
-        {
-          final ZonedDateTime now = ZonedDateTime.now();
-          userAuthToken.setLogoutAt(now);   // Setting the Logout Time of the user
-          return userAuthToken;
-        } else {
-          throw new SignOutRestrictedException("SGR-001", "User is not Signed in");
-        }
+  // login-logout auditing is done in user-auth-token-entity
+  public UserAuthTokenEntity signOut(String access_token) throws SignOutRestrictedException {
+    UserAuthTokenEntity userAuthToken =
+        userDao.getAuthToken(access_token); // Fetching authtoken entity
+    // Checking if the Access token entered matches the Access token in the DataBase and null check
+    if (userAuthToken != null && access_token.equals(userAuthToken.getAccess_token())) {
+      final ZonedDateTime now = ZonedDateTime.now();
+      userAuthToken.setLogoutAt(now); // Setting the Logout Time of the user
+      return userAuthToken;
+    } else {
+      throw new SignOutRestrictedException(
+          GenericExceptionCode.SGR_001_SIGNOUT.getCode(),
+          GenericExceptionCode.SGR_001_SIGNOUT.getDescription());
     }
-
+  }
 
   /*
- This method is used to delete the details of a signed in and Authorized user by an Admin user
-  */
-  public UserEntity deleteUserByUuid(final String userUuid, final String authorization) throws AuthorizationFailedException, UserNotFoundException {
+  This method is used to delete the details of a signed in and Authorized user by an Admin user
+   */
+  public UserEntity deleteUserByUuid(final String userUuid, final String authorization)
+      throws AuthorizationFailedException, UserNotFoundException {
 
     UserAuthTokenEntity userAuthTokenEntity = userDao.getAuthToken(authorization);
-    if(userAuthTokenEntity == null){
-      throw new AuthorizationFailedException("ATHR-001","User has not signed in");
+    // If accessToken does not exist in the Database,̥throws
+    // AuthorizationFailedException
+    if (userAuthTokenEntity == null) {
+      throw new AuthorizationFailedException(
+          GenericExceptionCode.ATHR_001.getCode(), GenericExceptionCode.ATHR_001.getDescription());
     }
 
-    //Check user signed out condition
+    // Checking user signed out condition
     final ZonedDateTime loggedOutTime = userAuthTokenEntity.getLogoutAt();
-    final ZonedDateTime now = ZonedDateTime.now();
-    if(loggedOutTime != null && now.isAfter(loggedOutTime)){
-      throw new AuthorizationFailedException("ATHR-002","User is signed out");
+    if (hasUserSignedOut(loggedOutTime)) {
+      throw new AuthorizationFailedException(
+          GenericExceptionCode.ATHR_002_USER_DELETE.getCode(),
+          GenericExceptionCode.ATHR_002_USER_DELETE.getDescription());
     }
     UserEntity user = userAuthTokenEntity.getUsers();
-    if(("nonadmin").equals(user.getRole())){
-      throw new AuthorizationFailedException("ATHR-003","Unauthorized Access, Entered user is not an admin");
+    if (("nonadmin").equals(user.getRole())) {
+      throw new AuthorizationFailedException(
+          GenericExceptionCode.ATHR_003_ADMIN.getCode(),
+          GenericExceptionCode.ATHR_003_ADMIN.getDescription());
     }
 
-    //Check if the user exists for the given uuid
+    // Check if the user exists for the given uuid
     UserEntity userToDelete = userDao.getUserbyUuid(userUuid);
 
-    if(userToDelete == null){
-      throw new UserNotFoundException("USR-001","User with entered uuid to be deleted does not exist");
+    if (userToDelete == null) {
+      throw new UserNotFoundException(
+          GenericExceptionCode.USR_001.getCode(), GenericExceptionCode.USR_001.getDescription());
     }
 
     return userDao.deleteUser(userToDelete);
   }
 
-    /*
-    This method is used to fetch all the details of a signed in and Authorized user
-     */
-   public UserEntity getUserDetails(final String userUuid, final String access_token) throws AuthorizationFailedException, UserNotFoundException {
-      final UserAuthTokenEntity userAuthToken = userDao.getAuthToken(access_token);
-      final UserEntity userEntity = userDao.getUserbyUuid(userUuid);
-      if (userAuthToken==null) {    // If accessToken does not exist in the Database
-        throw new AuthorizationFailedException("ATHR-001", "User has not Signed in");
-      } else if (userEntity==null) {    // If user UUID does not exist in the Database
-        throw new UserNotFoundException("USR-001","User with entered uuid does not exist");
-        // Checking if user has signed out
-      } else if (userAuthToken.getLogoutAt()!=null && userAuthToken.getLogoutAt().isBefore(ZonedDateTime.now())) {
-        throw new AuthorizationFailedException("ATHR-002", "User is signed out.Sign in first to get user details");
-      } else
-        return userEntity;
+  /*
+  This method is used to fetch all the details of a signed in and Authorized user. It takes the Access Token of the Logged-in
+  User and the User UUID from the Controller Method. It fetches the User Details from the DAO class and returns it to the Controller
+  method
+   */
+  public UserEntity getUserDetails(final String userUuid, final String access_token)
+      throws AuthorizationFailedException, UserNotFoundException {
+    final UserAuthTokenEntity userAuthToken = userDao.getAuthToken(access_token);
+    final UserEntity userEntity = userDao.getUserbyUuid(userUuid);
+    // If accessToken does not exist in the Database,̥throws
+    // AuthorizationFailedException
+    if (userAuthToken == null) {
+      throw new AuthorizationFailedException(
+          GenericExceptionCode.ATHR_001.getCode(), GenericExceptionCode.ATHR_001.getDescription());
     }
+    // If user UUID does not exist in the Database,throws UserNotFoundException
+    else if (userEntity == null) {
+      throw new UserNotFoundException(
+          GenericExceptionCode.USR_001.getCode(), GenericExceptionCode.USR_001.getDescription());
+    }
+    // Checking if user has signed out, then throws AuthorizationFailedException
+    else if (hasUserSignedOut(userAuthToken.getLogoutAt())) {
+      throw new AuthorizationFailedException(
+          GenericExceptionCode.ATHR_002_GET.getCode(),
+          GenericExceptionCode.ATHR_002_GET.getDescription());
+    } else return userEntity;
+  }
+
+  // Checks if the user has signed out by comparing if the current time is after the loggedOutTime
+  // received by the method
+  // Returns true if the current-time is after loggedOutTime(sign-out has happened), false otherwise
+  public boolean hasUserSignedOut(ZonedDateTime loggedOutTime) {
+    return (loggedOutTime != null && ZonedDateTime.now().isAfter(loggedOutTime));
+  }
 }
